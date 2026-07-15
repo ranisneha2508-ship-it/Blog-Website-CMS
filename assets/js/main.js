a92583270216dabed489214c4886f74006af7feb
Sneha Rani <ranisneha2508@gmail.com>
2026-07-15 23:39:24 +0530

    Complete homepage UI

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".testimonialSwiper", {
+
+    loop:true,
+
+
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+
+});
--- index.php
+++ index.php
@@ -3,34 +3,499 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <title>Blog Website CMS</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">    
+<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
-<div class="header">
-    <div class="container">
-        <div class="row d-flex align-items-center">
-            
-                <div class="logo">
-                    <h1>Finsweet</h1>
+    <header class="header">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="logo">
+                        <h1>Finsweet</h1>
+                    </div>
                 </div>
 
-<div class="nav">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Contact us</a></li>
-                    </ul>
+                <div class="col-lg-9 col-md-8 col-6">
+                    <nav class="nav-menu">
+                        <ul class="d-flex justify-content-end align-items-center list-unstyled mb-0">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <a href="#" class="btn-subscribe">Get Started</a>
+                        </ul>
+                    </nav>
                 </div>
+
+            </div>
+        </div>
+    </header>
+
+<div class="banner">
+    <div class="container">
+        <div class="row">
+            <div class="banner-content">
+            <div class="col-8">
+                <h2>Posted on <strong>startup</strong></h2>
+                <h1>Step-by-step guide to choosing great font pairs</h1>
+                <p class="details">By <span>James West </span> |  May 23, 2022 </p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                <a href="#" class="btn-primary">Read More ></a>
+            </div>
             </div>
         </div>
     </div>
 </div>
 
+<section class="posts">
+    <div class="container">
+    <div class="row d-flex justify-content-between">
+        <div class="col-lg-8 col-md-12 col-12">
+        <div class="featured-post">
+            <h2>Featured Post</h2>
 
+            <div class="post-card">
+
+                <img src="assets/images/featured.jpg" alt="">
+<div class="post-content">
+                <p class="meta">
+                    By <span2>John Doe</span2> |
+                    May 23, 2022
+                </p>
+
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</h3>
+
+                <p class="desc">
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+                </p>
+                <a href="#" class="btn-primary">Read More ></a>
+</div>
+            </div>
+        </div>
+        </div>
+<div class="col-lg-4 col-md-12 col-12">
+        <div class="all-posts">
+
+            <div class="heading">
+                <h2>All Posts</h2>
+                <a href="#">View All</a>
+            </div>
+
+            <div class="post-list">
+
+                <div class="post-item active">
+                     <p class="meta">
+                    By <span2>John Doe</span2> |
+                    May 23, 2022
+                </p>
+                <h4>8 Figma design systems that you can download for free today.</h4>
+                </div>
+
+                 <div class="post-item active">
+                     <p class="meta">
+                    By <span2>John Doe</span2> |
+                    May 23, 2022
+                </p>
+                <h4>8 Figma design systems that you can download for free today.</h4>
+                </div>
+ <div class="post-item active">
+                     <p class="meta">
+                    By <span2>John Doe</span2> |
+                    May 23, 2022
+                </p>
+                <h4>8 Figma design systems that you can download for free today.</h4>
+                </div>
+                 <div class="post-item active">
+                     <p class="meta">
+                    By <span2>John Doe</span2> |
+                    May 23, 2022
+                </p>
+                <h4>8 Figma design systems that you can download for free today.</h4>
+                </div>
+
+            </div>
+</div>
+        </div>
+</div>
+</div>
+</section>
+
+<section class="about">
+    <div class="container">
+
+        <div class="about-top-bar">
+            <div class="yellow-bar"></div>
+            <div class="purple-bar"></div>
+        </div>
+
+        <div class="about-content">
+            <div class="row d-flex align-items-center">
+
+                <div class="col-lg-6">
+                    <div class="about-box">
+                        <span>ABOUT US</span>
+                        <h2>
+                            We are a community of content writers who share their learnings
+                        </h2>
+                        <p>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.    
+                        </p>
+                        <a href="#">Read More ></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="mission-box">
+                        <span>OUR MISSION</span>
+                        <h3>
+                            Creating valuable content for creatives all around the world
+                        </h3>
+                        <p>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="category">
+    <div class="container">
+        <div class="section-title text-center">
+            <h2>Choose A Category</h2>
+        </div>
+        <div class="row g-4">
+
+            <div class="col-lg-3 col-md-6">
+                <div class="category-card">
+                    <div class="icon">
+                        <img src="assets/images/business.png" alt="">
+                    </div>
+
+                    <h3>Business</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="category-card">
+                    <div class="icon">
+                        <img src="assets/images/startup.png" alt="">
+                    </div>
+
+                    <h3>Startup</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="category-card">
+                    <div class="icon">
+                        <img src="assets/images/economy.png" alt="">
+                    </div>
+
+                    <h3>Economy</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="category-card">
+                    <div class="icon">
+                        <img src="assets/images/technology.png" alt="">
+                    </div>
+
+                    <h3>Technology</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+<section class="started">
+    <div class="container">
+
+        <div class="started-wrapper">
+
+            <div class="started-image">
+                <img src="assets/images/started.png" alt="Started">
+            </div>
+
+            <div class="started-content">
+                <span>WHY WE STARTED</span>
+                <h2>
+                    It started out as a simple idea and evolved into our passion
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                 </p>
+                <a href="#" class="btn-primary">
+                    Discover our story >
+                </a>
+            </div>
+        </div>
+
+    </div>
+</section>
+<section class="authors">
+    <div class="container">
+
+        <h2 class="text-center fw-bold mb-5">List of Authors</h2>
+
+        <div class="row g-4">
+
+            <div class="col-lg-3 col-md-6">
+                <div class="author-card text-center p-4">
+
+                    <img src="assets/images/author-1.png" class="img-fluid rounded-circle mb-3" alt="">
+
+                    <h4>Floyd Miles</h4>
+
+                    <p>Content Writer @Company</p>
+
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="author-card text-center p-4">
+
+                    <img src="assets/images/author-2.png" class="img-fluid rounded-circle mb-3" alt="">
+
+                    <h4>Dianne Russell</h4>
+
+                    <p>Content Writer @Company</p>
+
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+
+                </div>
+            </div>
+  <div class="col-lg-3 col-md-6">
+                <div class="author-card text-center p-4">
+
+                    <img src="assets/images/author-3.png" class="img-fluid rounded-circle mb-3" alt="">
+
+                    <h4> Jenny Wilson</h4>
+
+                    <p>Content Writer @Company</p>
+
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+
+                </div>
+            </div>
+              <div class="col-lg-3 col-md-6">
+                <div class="author-card text-center p-4">
+
+                    <img src="assets/images/author-4.png" class="img-fluid rounded-circle mb-3" alt="">
+
+                    <h4> Leslie Alexander</h4>
+
+                    <p>Content Writer @Company</p>
+
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+<section class="featured-in text-center ">
+    <div class="container">
+        <div class="row align-items-center">
+
+            <div class="col-lg-2">
+                <div class="featured-text">
+                    <p>We are</p>
+                    <h5>Featured in</h5>
+                </div>
+            </div>
+
+            <div class="col-lg-10">
+                <div class="featured-logo">
+                    <img src="assets/images/image.png" alt="">
+                    <img src="assets/images/image.png" alt="">
+                    <img src="assets/images/image.png" alt="">
+                    <img src="assets/images/image.png" alt="">
+                    <img src="assets/images/image.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="testimonial">
+    <div class="container">
+        <div class="testimonial-wrapper">
+            <div class="row align-items-center">
+                <div class="col-lg-4">
+                    <div class="testimonial-left">
+                        <span>TESTIMONIALS</span>
+                        <h2>What people say about our blog</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </div>
+</div>
+                <div class="col-lg-8">
+                    <div class="testimonial-right">
+                        <div class="swiper testimonialSwiper">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <h4>
+                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                  </h4>
+                                    <div class="client">
+                                        <img src="assets/images/client.png" alt="">
+                                        <div>
+                                            <h5>Jonathan Vallem</h5>
+                                            <span>New York, USA</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <h4>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    </h4>
+                                    <div class="client">
+                                        <img src="assets/images/client.png" alt="">
+                                        <div>
+                                            <h5>Jenny Wilson</h5>
+                                            <span>London, UK</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-btn">
+                            <div class="swiper-button-prev"></div>
+                           <div class="swiper-button-next"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="join">
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-lg-5 text-center">
+
+                <h2>Join our team to be a part of our story</h2>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                </p>
+
+                <a href="#" class="btn-primary">Join Now</a>
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="container">
+        <div class="footer-top d-flex justify-content-between align-items-center">
+            <div class="footer-logo">
+                <h3>{Finsweet</h3>
+            </div>
+            <nav>
+                <ul class="d-flex list-unstyled m-0">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="subscribe-box">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h2>
+                        Subscribe to our news letter to get latest updates and news
+                    </h2>
+                </div>
+                <div class="col-lg-6">
+                    <form class="d-flex gap-3">
+                        <input type="email"
+                        class="form-control"
+                        placeholder="Enter Your Email">
+                        <button class="btn-primary border-0">
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom d-flex justify-content-between align-items-center">
+            <div>
+                <p>
+                    Finsweet 118 2561 Fintown
+                </p>
+                <p>
+                    Hello@finsweet.com 020 7993 2905
+                </p>
+            </div>
+            <div class="social-icons">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js"></script>
 
 </body>
 </html>
